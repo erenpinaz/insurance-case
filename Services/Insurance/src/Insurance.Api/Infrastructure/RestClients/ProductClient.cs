94754adbe322572be4d68bbc641a763f22a29998
Nejdet Eren Pinaz <pinaz.eren@gmail.com>
2023-07-06 10:37:35 +0300

    remove logging from ProductClient

--- Services/Insurance/src/Insurance.Api/Infrastructure/RestClients/ProductClient.cs
+++ Services/Insurance/src/Insurance.Api/Infrastructure/RestClients/ProductClient.cs
@@ -2,25 +2,19 @@
 {
     public class ProductClient : BaseClient, IProductClient
     {
-        private readonly ILogger<ProductClient> _logger;
-
-        public ProductClient(HttpClient httpClient, ILogger<ProductClient> logger)
+        public ProductClient(HttpClient httpClient)
             : base(httpClient)
         {
-            _logger = logger;
+
         }
 
         public async Task<ProductDto> GetProductByIdAsync(int id)
         {
-            _logger.LogInformation("Requesting a product with id: {id}", id);
-
             return await GetAsync<ProductDto>($"products/{id}");
         }
 
         public async Task<ProductTypeDto> GetProductTypeByIdAsync(int id)
         {
-            _logger.LogInformation("Requesting a product type with id: {id}", id);
-
             return await GetAsync<ProductTypeDto>($"product_types/{id}");
         }
     }
